95617a1248e28f7c461aac4c69231ed252a29ce4
Jackylai01 <94669444+Jackylai01@users.noreply.github.com>
2023-01-04 13:41:39 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch 後端的api ->bot's response
 
-  const response = await fetch("http://localhost:8000", {
+  const response = await fetch("https://jackychatgpt.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
